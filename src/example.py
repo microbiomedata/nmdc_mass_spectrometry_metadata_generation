d86eb124d8f4f4bd9a0f0d9b3b1d0699d613c48f
olivia.hess <olivia.hess@pnnl.gov>
2025-02-03 12:08:13 -0500

    exmaple of how we will use credentials in the github actions

--- src/example.py
+++ src/example.py
@@ -1,6 +1,8 @@
+import os
 class Example:
     def __init__(self):
-        pass
+        self.api_key = os.getenv('API_KEY')
+        self.api_secret = os.getenv('API_SECRET')
     def calculate(self):
         """
         This function calculates 1 + 1 and returns the result. 
