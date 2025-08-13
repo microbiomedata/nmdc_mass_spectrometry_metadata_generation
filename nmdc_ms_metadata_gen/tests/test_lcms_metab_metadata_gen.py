e5472316b468df59f64607566508b3238e9da9c4
Katherine Heal <katherine.heal@pnnl.gov>
2025-08-13 11:02:11 -0700

    Re-instantiate test_lcsms_metab_biosample_gen

--- nmdc_ms_metadata_gen/tests/test_lcms_metab_metadata_gen.py
+++ nmdc_ms_metadata_gen/tests/test_lcms_metab_metadata_gen.py
@@ -70,7 +70,7 @@ def test_lcms_metab_metadata_gen_rerun():
     assert os.path.exists(output_file)
 
 
-def xtest_lcms_metab_biosample_gen():
+def test_lcms_metab_biosample_gen():
     current_directory = os.path.dirname(__file__)
     csv_file_path = os.path.join(
         current_directory,
