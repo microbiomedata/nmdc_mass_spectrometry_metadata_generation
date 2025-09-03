552c4cc8f8b16c140c3ba403a91156b42566aa6e
olivia.hess <olivia.hess@pnnl.gov>
2025-09-03 13:46:52 -0400

    test updates

--- nmdc_ms_metadata_gen/tests/validate_yaml_outline.py
+++ nmdc_ms_metadata_gen/tests/validate_yaml_outline.py
@@ -5,21 +5,21 @@ import sys
 # clarifying path variable for relative imports
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 
-from metadata_gen_scripts.data_parser import YamlSpecifier
-from metadata_gen_scripts.material_processing_gen import (
+from nmdc_ms_metadata_gen.material_processing_generator import (
     MaterialProcessingMetadataGenerator,
 )
+from nmdc_ms_metadata_gen.metadata_parser import YamlSpecifier
 
 
 def validate_yaml_outline(yaml_outline_path=str):
     """
     test to make sure yaml will generate valid json ()
-    command line example : python metadata_gen_scripts/tests/validate_yaml_outline.py --yaml_outline_path='studies/emp500_sty-11-547rwq94/500_metabolites_bulk_feces.yaml'
+    command line example : python nmdc_ms_metadata_gen/tests/validate_yaml_outline.py --yaml_outline_path='studies/emp500_sty-11-547rwq94/500_metabolites_bulk_feces.yaml'
     """
 
     generator = MaterialProcessingMetadataGenerator(
         yaml_outline_path=yaml_outline_path,
-        config_path="metadata_gen_scripts/config.yaml",
+        config_path="nmdc_ms_metadata_gen/config.yaml",
         study_id="sdjklfdjsf",  # doesn't matter, wont be called on
         output_path="sdjfkldsjf",  # doesn't matter, wont be called on
         sample_to_dg_mapping_path="jdksldjfs",  # doesn't matter, won't be called on
